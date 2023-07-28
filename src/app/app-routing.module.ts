c7ba2429a7dfd9cfc30b54030a2b2b633edcdd74
Dipin <dipinkarunakaran6@gmail.com>
2023-07-29 00:35:46 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'category',component:SingleCategoryComponent},
+  {path:'about',component:AboutUsComponent},
+
+  
+  {path:'post',component:SinglePostComponent},
+  {path:'terms',component:TermsAndConditionComponent},
+  {path:'contact',component:ContactUsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
